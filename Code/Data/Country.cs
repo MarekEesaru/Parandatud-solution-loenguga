bd179a1178562cf386fba9dc8ef58001433c6bb6
Marek Eesaru <mareke@taltech.ee>
2026-02-26 00:22:29 +0200

    Muutsin paar asja Country.cs-is

--- Code/Data/Country.cs
+++ Code/Data/Country.cs
@@ -6,7 +6,8 @@ public class  Country
 {
     public int Id { get; set; }
     public string Name { get; set; }
-    public string Short { get; set; } //EST
+    public string Continent { get; set; }
+    public string Language { get; set; }
 }
 
 
