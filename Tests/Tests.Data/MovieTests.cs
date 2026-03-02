09cbb15874d3a51b1afd53434d418e3a9d358722
Marek Eesaru <mareke@taltech.ee>
2026-03-02 18:48:51 +0200

    parandasin testid ära ja tegin nad lihtsamaks

--- Tests/Tests.Data/MovieTests.cs
+++ Tests/Tests.Data/MovieTests.cs
@@ -1,18 +1,16 @@
 ﻿using Abc.Data;
-
 namespace Abc.Tests.Data
 {
     [TestClass]
-    public sealed class MovieTests
+    public sealed class MovieTests : BaseTests<Movie> { }
+    [TestClass]
+    public class CountryTests : BaseTests<Country> { }
+    [TestClass]
+    public class CurrencyTests : BaseTests<Currency> { }
+    public abstract class BaseTests<TClass> where TClass : class, new()
     {
-        private Movie movie;
-
-        [TestInitialize] public void Initialize() => movie = new Movie();
-
-        [TestMethod]
-        public void CanCreateTest()
-        {
-            Assert.IsNotNull(movie);
-        }
+        protected TClass obj;
+        [TestInitialize] public void Initialize() => obj = new TClass();
+        [TestMethod] public void CanCreateTest() => Assert.IsNotNull(obj);
     }
 }
