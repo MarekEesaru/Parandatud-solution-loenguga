a97d5d990ebf03cbe53e43c1453a345c042c10b3
Marek Eesaru <mareke@taltech.ee>
2026-04-23 23:24:31 +0300

    uus migratsioon, korrastasin koodi, parandasin create ja edit lehed ära

--- Code/Data/Money.cs
+++ Code/Data/Money.cs
@@ -9,7 +9,7 @@ namespace Abc.Data
     public class Money : BaseEntity
     {
         public decimal Amount { get; set; }
-        public Guid CurrencyId { get; set; }
+        public Guid? CurrencyId { get; set; }
         public Currency Currency { get; set; }
     }
 }
