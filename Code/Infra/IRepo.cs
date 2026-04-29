86bc4ac0eef74b30767539cf66641205a9a3772e
Marek Eesaru <mareke@taltech.ee>
2026-04-29 20:46:03 +0300

    Query klassi tegemine

--- Code/Infra/IRepo.cs
+++ Code/Infra/IRepo.cs
@@ -2,19 +2,6 @@
 using Abc.Data.Common;
 
 namespace Abc.Infra;
-public sealed class Query(Dictionary<string, string> d = null)
-{
-    public static int[] PageSizes => [7, 15, 25, 50, 100];
-    public int Page => toInt(get(nameof(Page)), 1);
-    public int PageSize => toInt(get(nameof(PageSize)), PageSizes[0]);
-    public string SortBy => get(nameof(SortBy));
-    public string SortDir => get(nameof(SortDir));
-    public string SearchBy => get(nameof(SearchBy));
-    public string SearchStr => get(nameof(SearchStr));
-    public string Selected => get(nameof(Selected));
-    private string get(string name) => (d ?? []).TryGetValue(name, out var s) ? s : null;
-    private static int toInt(string s, int def) => int.TryParse(s, out var i) ? i : def;
-}
 public interface IRepo<TEntity> where TEntity : BaseEntity
 {
     Task<TEntity> GetAsync(Guid id);
